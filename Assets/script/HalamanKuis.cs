61cafc11d2fb98426e76672007b241574f9c8084
fadil <afadilah358@gmail.com>
2024-07-21 02:03:07 +0800

    Kuis

---
+++ Assets/script/HalamanKuis.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HalamanKuis : MonoBehaviour
+{
+    public void TombolBenarKuis(){
+        SceneManager.LoadScene("HalamanKuis2");
+    }
+}
