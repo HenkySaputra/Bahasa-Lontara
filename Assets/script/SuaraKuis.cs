10d554caf954a136712b51801f12d0c036b3ce6e
fadil <afadilah358@gmail.com>
2024-07-21 15:56:45 +0800

    tambah suara kuis

---
+++ Assets/script/SuaraKuis.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuaraKuis : MonoBehaviour
+{
+   public AudioSource suara;
+
+   public void mainkan(){
+        suara.Play();
+   }
+}
